bfbca59ef4b6e86527d016059d9eeab180eeac62
JatinShimpi <shimpijatin184@gmail.com>
2024-11-29 01:22:13 +0530

    ADDED SUPABASE SERVICE AND OTHER COMPONENTS

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "himcbaynxhchayjtxokj.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabim-images/**",
+      },
+    ],
+  },
   /* config options here */
 };
 
